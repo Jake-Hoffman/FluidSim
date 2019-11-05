64e812765cad27d80105fb325fa213448bcd2249
Jacob Hoffman <jaketh19@gmail.com>
2019-11-05 17:52:59 -0500

    Added project files

---
+++ src/game/Main.java
@@ -0,0 +1,17 @@
+package game;
+
+import engine.GameEngine;
+import engine.IGameLogic;
+
+public class Main {
+
+    public static void main(String[] args){
+        try {
+            GameEngine gameEng = new GameEngine("Fluid Simulation", 500, 500, 100);
+            gameEng.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
